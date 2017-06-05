161569e4fc8c5840944710d8a646f97de5f67da5
jhsware <sebastian@urbantalk.se>
2017-06-05 12:03:18 +0200

    Expose more widgets

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 var FormRows = require('./dist/FormRows').FormRows
 
-var InputField = require('./dist/widgets/InputField').default
+var AutoCompleteWidget = require('./dist/widgets/AutoComplete').default
+var AnyOfWidget = require('./dist/widgets/AnyOf').default
+var CheckboxWidget = require('./dist/widgets/BoolField').default
+var InputWidget = require('./dist/widgets/InputField').default
+var ListWidget = require('./dist/widgets/ListField').default
+var MultiSelectWidget = require('./dist/widgets/MultiSelectField').default
+var ObjectWidget = require('./dist/widgets/ObjectField').default
+var PasswordWidget = require('./dist/widgets/PasswordField').default
+var SelectWidget = require('./dist/widgets/SelectField').default
+var TextAreaWidget = require('./dist/widgets/TextAreaField').default
+
+var CustomWidget = require('./dist/CustomWidget').default
+
 var SelectAsyncBaseWidget = require('./dist/widgets/SelectAsyncBaseWidget').default
 require('./dist/widgets/BoolField')
 require('./dist/widgets/PasswordField')
@@ -13,10 +25,20 @@ require('./dist/widgets/MultiSelectField')
 require('./dist/widgets/AnyOf')
 
 module.exports = {
-    FormRows: FormRows,
+    CustomWidget,
+    FormRows,
     interfaces: require('./dist/interfaces'),
     widgets: {
-        InputField: InputField,
-        SelectAsyncBaseWidget: SelectAsyncBaseWidget
+        AutoCompleteWidget,
+        AnyOfWidget,
+        CheckboxWidget,
+        InputWidget,
+        ListWidget,
+        MultiSelectWidget,
+        ObjectWidget,
+        PasswordWidget,
+        SelectWidget,
+        TextAreaWidget,
+        SelectAsyncBaseWidget
     } 
 }
