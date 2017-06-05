ab581225b2cc1b44f6c24ebc0821f1e99d1a9bae
jhsware <sebastian@urbantalk.se>
2017-06-05 17:25:51 +0200

    Autocomplete now has keyboard navigation and type ahead

--- lib/widgets/AutoComplete.jsx
+++ lib/widgets/AutoComplete.jsx
@@ -39,26 +39,56 @@ class AutocompleteItem extends Component {
     }
 
     render () {
-        return <div className="InfernoFormlib-AutocompleteItem" onClick={this.didClick}>{this.props.title}</div>
+        let cls = {
+            'InfernoFormlib-AutocompleteItem': true,
+            'InfernoFormlib-AutocompleteItem--Highlight': this.props.isMarked
+        }
+        return <div className={classNames(cls)} onClick={this.didClick}>{this.props.title}</div>
     }
 }
 
-function _getOptionsAsync (nextProps) {
-    // TODO: How do we supply these because they could be used to get options
-    const options = undefined
-    const context = undefined
-    
-    this.props.adapter.context.getOptionsAsync(nextProps.value, options, context)
+function _getOptionsAsync (value, options, context) {
+    const currentMarked = this.state.options[this.state.markAtIndex]
+
+    this.props.adapter.context.getOptionsAsync(value, options, context)
         .then((results) => {
+            // Maintain selection when updating list if possible
+            const markAtIndex = currentMarked && results.reduce((curr, next, index) => curr === undefined && next.name === currentMarked.name ? index : curr, undefined)
+
             this.setState({
-                options: results
+                options: results,
+                markAtIndex: markAtIndex || 0 // Default to select top most option
             })  
         })
         .catch(() => {
             this.setState({
-                options: [{ name: nextProps.value, title: 'Fel när data laddades...'}]
+                options: [{ name: value, title: 'Fel när data laddades...'}],
+                markAtIndex: undefined
             })            
         })
+}
+
+
+function TypeAhead (props) {
+    if (!props.markedValue) {
+        return null
+    } else {
+        const text = props.text
+        const suffix = props.markedValue.title.substr(text.length)
+
+        return (
+            <div className="InfernoFormlib-AutoComplete-TypeAhead">
+                <span className="InfernoFormlib-AutoComplete-TypeAheadPrefix">{text}</span>
+                <span className="InfernoFormlib-AutoComplete-TypeAheadSuffix">{suffix}</span>
+            </div>
+        )
+    }
+}
+
+const _actionKeys = {
+    ArrowDown: true,
+    ArrowUp: true,
+    Enter: true
 }
 
 class AutoCompleteBaseWidget extends Component {
@@ -67,9 +97,11 @@ class AutoCompleteBaseWidget extends Component {
 
         this.state = {
             text: props.value,
-            options: []
+            options: [],
+            markAtIndex: undefined
         }
 
+        this.didGetHotKey = this.didGetHotKey.bind(this)
         this.didGetInput = this.didGetInput.bind(this)
         this.didGetChange = this.didGetChange.bind(this)
         this.didSelect = this.didSelect.bind(this)
@@ -80,7 +112,7 @@ class AutoCompleteBaseWidget extends Component {
         const options = undefined
         const context = undefined
 
-        if (nextProps.value !== this.props.value) {
+        if (nextProps.value && nextProps.value !== this.props.value) {
             this.props.adapter.context.getOptionTitleAsync(nextProps.value, options, context)
               .then((title) => {
                   this.setState({
@@ -98,17 +130,55 @@ class AutoCompleteBaseWidget extends Component {
         })
     }
 
+    componentDidMount () {
+        document.addEventListener('keydown', this.didGetHotKey, false)
+    }
+
+    componentWillUnmount () {
+        document.removeEventListener('keydown', this.didGetHotKey)
+    }
+
+    didGetHotKey (e) {
+        // Use for navigation and enter only!
+        console.log('DID PRESS: ' + e)
+        if (_actionKeys[e.key] && this.state.markAtIndex !== undefined) {
+            // So we got a hotkey
+            if (this._vNode.dom.getElementsByClassName('InfernoFormlib-TextField')[0] === e.target) {
+                // And it was fired in our input box
+                if (e.key === 'ArrowUp') {
+                    this.setState({
+                        markAtIndex: this.state.markAtIndex <= 0 ? this.state.options.length - 1 : this.state.markAtIndex - 1
+                    })
+                    e.preventDefault()
+                } else if (e.key === 'ArrowDown') {
+                    this.setState({
+                        markAtIndex: this.state.markAtIndex >= (this.state.options.length - 1) ? 0 : this.state.markAtIndex + 1
+                    })
+                    e.preventDefault()
+                } else if (e.key === 'Enter') {
+                    if (this.state.markAtIndex !== undefined) {
+                        this.didSelect(this.state.options[this.state.markAtIndex])
+                    }
+                    e.preventDefault()
+                }
+            }
+        }
+    }
+
     didGetInput (e) {
         // TODO: How do we supply these because they could be used to get options
-        const options = undefined
-        const context = undefined
+        const options = {
+            search: e.target.value
+        }
 
         if (e.target.value !== this.state.text) {
-            _getOptionsAsync.call(this, e.target.value, options, context)
+            const context = undefined
+            _getOptionsAsync.call(this, undefined, options, context)
         }
 
         this.setState({
-            text: e.target.value
+            text: e.target.value,
+            markAtIndex: e.target.value ? this.state.markAtIndex : undefined
         })
     }
 
@@ -118,7 +188,11 @@ class AutoCompleteBaseWidget extends Component {
 
     didSelect (value) {
       const field = this.props.adapter.context
-      this.props.onChange(this.props.propName, field.valueType.fromString(this.state.value))
+      this.props.onChange(this.props.propName, field.valueType.fromString(value.name))
+      this.setState({
+          options: [],
+          markAtIndex: undefined
+      })
     }
 
     render () {
@@ -129,11 +203,15 @@ class AutoCompleteBaseWidget extends Component {
             "InfernoFormlib-TextField--readonly": field.readOnly
         }
 
+        // Only show options if we have entered text
+        const options = this.state.text ? this.state.options : []
+
         return <div className="InfernoFormlib-AutoCompleteField">
           <input className={classNames(cls)} type="text" placeholder={field.placeholder} readonly={field.readOnly && 'true'} value={this.state.text} 
                  onChange={this.didGetChange} onInput={this.didGetInput} />
+          {this.state.text && <TypeAhead text={this.state.text} markedValue={this.state.options[this.state.markAtIndex]} />}
           <div className="InfernoFormlib-AutoCompleteItemContainer">
-            {this.state.options.map((item) => <AutocompleteItem key={item.name} value={item.name} title={item.title} onSelect={this.didSelect} />)}
+            {options.map((item, index) => <AutocompleteItem key={index} value={item} title={item.title} isMarked={index === this.state.markAtIndex} onSelect={this.didSelect} />)}
           </div>
         </div>
     }
