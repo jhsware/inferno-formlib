e6f082499627e790a95a3ca5efe8ee2e6e83f528
jhsware <sebastian@urbantalk.se>
2017-06-16 12:58:26 +0200

    Added DynamicSelectField

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+'use strict'
 var FormRows = require('./dist/FormRows').FormRows
 
 var AutoCompleteWidget = require('./dist/widgets/AutoComplete').default
 var AnyOfWidget = require('./dist/widgets/AnyOf').default
 var CheckboxWidget = require('./dist/widgets/BoolField').default
+var DynamicSelectWidget = require('./dist/widgets/DynamicSelectField').default
 var InputWidget = require('./dist/widgets/InputField').default
 var ListWidget = require('./dist/widgets/ListField').default
 var MultiSelectWidget = require('./dist/widgets/MultiSelectField').default
@@ -10,19 +12,11 @@ var ObjectWidget = require('./dist/widgets/ObjectField').default
 var PasswordWidget = require('./dist/widgets/PasswordField').default
 var SelectWidget = require('./dist/widgets/SelectField').default
 var TextAreaWidget = require('./dist/widgets/TextAreaField').default
+var SelectAsyncBaseWidget = require('./dist/widgets/SelectAsyncBaseWidget').default
 
 var CustomWidget = require('./dist/CustomWidget').default
 
-var SelectAsyncBaseWidget = require('./dist/widgets/SelectAsyncBaseWidget').default
-require('./dist/widgets/BoolField')
-require('./dist/widgets/PasswordField')
-require('./dist/widgets/TextAreaField')
-require('./dist/widgets/ObjectField')
-require('./dist/widgets/ListField')
 require('./dist/widgets/FormRow')
-require('./dist/widgets/SelectField')
-require('./dist/widgets/MultiSelectField')
-require('./dist/widgets/AnyOf')
 
 module.exports = {
     CustomWidget,
@@ -32,6 +26,7 @@ module.exports = {
         AutoCompleteWidget,
         AnyOfWidget,
         CheckboxWidget,
+        DynamicSelectWidget,
         InputWidget,
         ListWidget,
         MultiSelectWidget,
---
+++ lib/widgets/DynamicSelectField.jsx
@@ -0,0 +1,63 @@
+'use strict'
+/*
+
+    To use this input widget adapter you need to register it with your
+    adapter registry.
+
+*/
+import Inferno from 'inferno'
+import Component from 'inferno-component'
+
+import { createAdapter, globalRegistry } from 'component-registry'
+
+import { interfaces } from 'isomorphic-schema'
+import { IInputFieldWidget }  from '../interfaces'
+import classNames from 'classnames'
+
+// Placeholder
+
+// Placeholder
+
+class DynamicSelectFieldWidget extends Component {
+    constructor (props) {
+        super(props)
+
+        this.didGetChange = this.didGetChange.bind(this)
+    }
+
+    componentWillReceiveProps (nextProps) {
+        this.setState({
+            value: nextProps.value
+        })
+    }
+
+    didGetChange (e) {
+        const field = this.props.adapter.context
+        this.props.onChange(this.props.propName, field.fromString(e.target.value))
+    }
+
+    render () {
+        const field = this.props.adapter.context
+
+        const options = field.getOptions(this.props.value, this.props.options)
+
+        const cls = {
+            "InfernoFormlib-SelectField": true,
+            "InfernoFormlib-SelectField--readonly": field.readOnly
+        }
+
+        return <select className={classNames(cls)} type="text" readonly={field.readOnly && 'true'} value={this.props.value} 
+                    onChange={this.didGetChange}>
+            {field.placeholder && <option value="">{field.placeholder}</option>}
+            {options.map((item) => <option value={item.name}>{item.title}</option>)}
+        </select>
+    }
+}
+
+export default DynamicSelectFieldWidget
+
+createAdapter({
+    implements: IInputFieldWidget,
+    adapts: interfaces.IDynamicSelectField,
+    Component: DynamicSelectFieldWidget,
+}).registerWith(globalRegistry)
