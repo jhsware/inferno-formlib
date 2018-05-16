5cba676b51141e1bd3aa67f57729b64c40e4e526
jhsware <sebastian@urbantalk.se>
2018-05-16 16:15:51 +0200

    Fixed bugs related to registration and opening of popup

--- test/browser/src/DateTimePage.js
+++ test/browser/src/DateTimePage.js
@@ -5,10 +5,20 @@ import FormText from 'inferno-bootstrap/lib/Form/FormText'
 import FormGroup from 'inferno-bootstrap/lib/Form/FormGroup'
 import Label from 'inferno-bootstrap/lib/Form/Label'
 
+import Row from 'inferno-bootstrap/dist/Row'
+import Col from 'inferno-bootstrap/dist/Col'
+
 const dateTimeField = new DateTimeField({
     label: 'DateTime Field',
     placeholder: 'Click to select date...',
     help: 'Enter a nice date and time',
+    required: true
+})
+
+const secondDateTimeField = new DateTimeField({
+    label: 'Second DateTime Field',
+    placeholder: 'Click to select date...',
+    help: 'The popup should point to the correct field',
     required: true
 })
 
@@ -29,20 +39,43 @@ export default class Page extends Component {
             context: dateTimeField
         }
 
+        const secondDummyAdapter = {
+            context: secondDateTimeField
+        }
+
         return (
             <div className="TestContainer">
                 <h1>DateTime Widget</h1>
-                <FormGroup>
-                    <Label id="dateField">{dateTimeField.label}</Label>
-                    <div className="InfernoFormlib-RowFieldContainer">
-                        <DateTimeFieldWidget
-                            namespace={['value']}
-                            adapter={dummyAdapter}
-                            onChange={() => null} />
-                    </div>
-                    {this.state.validationError ? <ErrorMsg validationError={this.state.validationError} submitted={this.state.submitted} /> : null}
-                    {dateTimeField.help && <FormText className="text-muted" for="dateField">{dateTimeField.help}</FormText>}
-                </FormGroup>
+                <Row>
+                    <Col>
+                        <FormGroup>
+                            <Label id="dateField">{dateTimeField.label}</Label>
+                            <div className="InfernoFormlib-RowFieldContainer">
+                                <DateTimeFieldWidget
+                                    namespace={['value']}
+                                    adapter={dummyAdapter}
+                                    onChange={() => null} />
+                                {this.state.validationError ? <ErrorMsg validationError={this.state.validationError} submitted={this.state.submitted} /> : null}
+                                {dateTimeField.help && <FormText className="text-muted" for="dateField">{dateTimeField.help}</FormText>}
+                            </div>
+                        </FormGroup>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <FormGroup>
+                            <Label id="secondDateField">{secondDateTimeField.label}</Label>
+                            <div className="InfernoFormlib-RowFieldContainer">
+                                <DateTimeFieldWidget
+                                    namespace={['secondValue']}
+                                    adapter={secondDummyAdapter}
+                                    onChange={() => null} />
+                                {this.state.validationError ? <ErrorMsg validationError={this.state.validationError} submitted={this.state.submitted} /> : null}
+                                {secondDateTimeField.help && <FormText className="text-muted" for="dateField">{secondDateTimeField.help}</FormText>}
+                            </div>
+                        </FormGroup>
+                    </Col>
+                </Row>
             </div>
         )
     }
