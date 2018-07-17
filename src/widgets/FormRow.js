9d69599c7794c59ca50269ae428b51adad8aa83c
jhsware <sebastian@urbantalk.se>
2018-07-17 16:36:35 +0200

    Expose support function

--- src/widgets/FormRow.js
+++ src/widgets/FormRow.js
@@ -238,4 +238,4 @@ createAdapter({
     Component: CheckboxRow
 }).registerWith(globalRegistry)
 
-export { CheckboxRow, ObjectRow, Row, ErrorMsg, HelpMsg, Label }
+export { CheckboxRow, ObjectRow, Row, ErrorMsg, HelpMsg, Label, unpackInvariantErrors }
