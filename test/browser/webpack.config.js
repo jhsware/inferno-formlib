edba4b0dec32c96f518518ffaacf91c2e993d947
jhsware <sebastian@urbantalk.se>
2018-11-05 14:31:47 +0100

    Change test webpack config

--- test/browser/webpack.config.js
+++ test/browser/webpack.config.js
@@ -15,27 +15,7 @@ module.exports = {
     rules: [{
       test: /\.jsx?$/,
       exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          "presets": [
-            ["@babel/env", { "modules": false }]
-          ],
-          "plugins": [
-            "@babel/proposal-class-properties",
-            "@babel/transform-runtime",
-            "@babel/transform-spread",
-            "add-module-exports",
-            "@babel/syntax-jsx",
-            [
-              "babel-plugin-inferno",
-              {
-                "imports": true
-              }
-            ]
-          ]
-        }
-      }
+      use: ['babel-loader']
     }]
   }
 }
